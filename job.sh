73d61b1c1897708f0e3600480e8dc51a48eb23fe
Mohammad Hadi Salari <salari.m1375@gmail.com>
2020-11-27 14:16:07 -0800

    Add job script

---
+++ job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --mail-user=salari.m1375@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --account=def-mori_cpu
+#SBATCH --job-name=MDS
+#SBATCH --output=%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --time=11:59:00
+#SBATCH --mem=100G
+
+cd $SLURM_TMPDIR
+cp -r ~/scratch/polygon .
+cd polygon
+
+module load python/3.7 cuda/10.0
+virtualenv --no-download venv
+source venv/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r requirements.txt
+
+python main.py
+
+
