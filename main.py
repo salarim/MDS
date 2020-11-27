25dc179890a61757d668bfd3369b52449cc08d4f
Mohammad Hadi Salari <salari.m1375@gmail.com>
2020-11-27 14:07:24 -0800

    Move try-except

--- main.py
+++ main.py
@@ -11,29 +11,29 @@ from methods import find_mds_iterative, \
 def main():
     benchmarks = pd.read_csv('benchmarks.csv')
     for index, row in benchmarks.iterrows():
-        if row['format'] == 'mtx':
-            loader = MtxGraphLoader(row['url'])
-        elif row['format'] == 'txt':
-            loader = TxtGraphLoader(row['url'])
-        elif row['format'] == 'dat':
-            loader = DatGraphLoader(row['instance'], row['url'])
-        else:
-            continue
-        
-        adj_matrix = loader.get_adj_matrix()
-
-        sol = []
-        rnds = np.random.rand(adj_matrix.shape[0])
         try:
+            if row['format'] == 'mtx':
+                loader = MtxGraphLoader(row['url'])
+            elif row['format'] == 'txt':
+                loader = TxtGraphLoader(row['url'])
+            elif row['format'] == 'dat':
+                loader = DatGraphLoader(row['instance'], row['url'], 0)
+            else:
+                continue
+        
+            adj_matrix = loader.get_adj_matrix()
+
+            sol = []
+            rnds = np.random.rand(adj_matrix.shape[0])
             sol.append(len(find_mds_iterative(adj_matrix, 5, rnds)))
             sol.append(len(find_mds_max_degree_count(adj_matrix, 5, rnds)))
-            sol.append(len(find_mds_max_count_seprate_neigh(adj_matrix, 5, rnds)))
+            # sol.append(len(find_mds_max_count_seprate_neigh(adj_matrix, 5, rnds)))
+        
+            print(row['instance'], adj_matrix.shape, sol, flush=True)
+            if row['format'] != 'dat' and index != len(benchmarks.index)-1:
+                loader.clean()
         except Exception as e:
             print(e)
-        
-        print(row['instance'], adj_matrix.shape, sol)
-        if row['format'] != 'dat' and index != len(benchmarks.index)-1:
-            loader.clean()
 
 if __name__ == '__main__':
     main()
